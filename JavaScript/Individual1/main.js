2e33a4e3bfd04646e10a9d5f85a135114adacb56
caxaexe <149768960+caxaexe@users.noreply.github.com>
2024-05-16 10:37:21 +0300

    Update main.js

--- JavaScript/Individual1/main.js
+++ JavaScript/Individual1/main.js
@@ -29,8 +29,8 @@ class TransactionAnalyzer {
      * @returns {Array<string>} Array of unique transaction types.
      */
     getUniqueTransactionType() {
-    const types = new Set(this.transactions.map(transaction => transaction.transaction_type)); // map извлекает типы из каждого объекта, set удаляет дубликаты и определяет уникальные значения
-    return Array.from(types); // преобразует объект set types обратно в массив
+    const types = new Set(this.transactions.map(transaction => transaction.transaction_type));
+    return Array.from(types);
     }
 
     /**
@@ -38,7 +38,7 @@ class TransactionAnalyzer {
      * @returns {number} Total amount of all transactions.
      */
     calculateTotalAmount() {
-        return this.transactions.reduce((total, transaction) => total + transaction.transaction_amount, 0); // reduce обрабатывает массив и сводит его к одному значению
+        return this.transactions.reduce((total, transaction) => total + transaction.transaction_amount, 0); 
     }
 
     /**
@@ -50,14 +50,14 @@ class TransactionAnalyzer {
      */
     calculateTotalAmountByDate(year, month, day) {
         let totalAmount = 0;
-        for (const transaction of this.transactions) { // позволяет перебирать каждую транзакцию
-            const transactionDate = new Date(transaction.transaction_date); // создаю объект Date, чтобы преобразовать строку ..._date в объект даты для сравнения с заданной датой
+        for (const transaction of this.transactions) {
+            const transactionDate = new Date(transaction.transaction_date);
             if (
-                transactionDate.getFullYear() === year && // проверка соответствует ли дата транзакции заданной
-                transactionDate.getMonth() === month - 1 && // -1 тк начинается с 0
+                transactionDate.getFullYear() === year &&
+                transactionDate.getMonth() === month - 1 &&
                 transactionDate.getDate() === day
             ) {
-                totalAmount += transaction.transaction_amount; // если проверка прошла, то + к общей сумме totalAmount
+                totalAmount += transaction.transaction_amount; 
             }
         }
         return totalAmount;
@@ -69,7 +69,7 @@ class TransactionAnalyzer {
      * @returns {Array<Object>} Array of transactions with the specified type.
      */
     getTransactionByType(type) {
-        return this.transactions.filter(t => t.transaction_type === type); // filter создает новый массив с элементами, прошедшими проверку, t представляет каждый элемент массива
+        return this.transactions.filter(t => t.transaction_type === type);
     }
 
     /**
@@ -79,11 +79,11 @@ class TransactionAnalyzer {
      * @returns {Array<Object>} Array of transactions within the specified date range.
      */
     getTransactionsInDateRange(startDate, endDate) {
-        const start = new Date(startDate); // создаются два объекта, Date интерпретирует вводимые данные, как допустимую дату
+        const start = new Date(startDate);
         const end = new Date(endDate);
-        return this.transactions.filter(t => { // filter перебирает каждый элемент t в массиве и сохраняет только те, которые подходят под true
-            const date = new Date(t.transaction_date); // извлекается часть даты из данных транзакций в date
-            return date >= start && date <= end; // проверка дат
+        return this.transactions.filter(t => {
+            const date = new Date(t.transaction_date);
+            return date >= start && date <= end;
         });
     }
 
@@ -93,7 +93,7 @@ class TransactionAnalyzer {
      * @returns {Array<Object>} Array of transactions from the specified merchant.
      */
     getTransactionsByMerchant(merchantName) {
-        return this.transactions.filter(t => t.merchant_name === merchantName); // проверка совпадают ли вводимые данные с данными из тразакций
+        return this.transactions.filter(t => t.merchant_name === merchantName);
     }
 
     /**
@@ -101,7 +101,7 @@ class TransactionAnalyzer {
      * @returns {number} Average transaction amount.
      */
     calculateAverageTransactionAmount() {
-        return this.calculateTotalAmount() / this.transactions.length; // делит общую сумму всех транзакций на количество транзакций и определяет среднюю сумму транзакций
+        return this.calculateTotalAmount() / this.transactions.length;
     }
 
     /**
@@ -111,7 +111,7 @@ class TransactionAnalyzer {
      * @returns {Array<Object>} Array of transactions within the specified amount range.
      */
     getTransactionsByAmountRange(minAmount, maxAmount) {
-        return this.transactions.filter(t => t.transaction_amount >= minAmount && t.transaction_amount <= maxAmount); // определяет соответствие минимума и максимума с данными из транзакции (t_amount)
+        return this.transactions.filter(t => t.transaction_amount >= minAmount && t.transaction_amount <= maxAmount);
     }
 
     /**
@@ -119,8 +119,7 @@ class TransactionAnalyzer {
      * @returns {number} Total debit amount.
      */
     calculateTotalDebitAmount() {
-        return this.getTransactionByType("debit").reduce((sum, t) => sum + t.transaction_amount, 0); // sum накапливает итоговую сумму и к ней прибавляется каждую итерацию текущая сумма транзакции
-        // reduce сжимает весь массив дебетовых транзакций в одно значение
+        return this.getTransactionByType("debit").reduce((sum, t) => sum + t.transaction_amount, 0);
     }
 
     /**
@@ -130,12 +129,10 @@ class TransactionAnalyzer {
     findMostTransactionsMonth() {
         const counts = {}; // пустой объект-счетчик
         for (const t of this.transactions) {
-            const month = new Date(t.transaction_date).getMonth(); // в month извлекается месяц из t_date, а метод getMonth получает месяц от 0 (янв) до 11 (дек)
-            counts[month] = (counts[month] || 0) + 1; // обращение к ключу month из counts, если counts[month] равен undefined, возвращает 0, если наоборот, то содержмое counts[month]
-            // +1 увеличивает значение счетчика на 1 для данного month
+            const month = new Date(t.transaction_date).getMonth();
+            counts[month] = (counts[month] || 0) + 1;
         }
-        return Object.keys(counts).reduce((a, b) => (counts[a] > counts[b] ? a : b)); // object.keys преобразует объект counts в массив его ключей (месяцев), reduce последовательно сравнивает месяца и определяет с наибольшим количеством транзакций
-        // ? сравнивается колво текущего месяца(а) и следующего (b), если true, то возвращает месяц а, если false, то b
+        return Object.keys(counts).reduce((a, b) => (counts[a] > counts[b] ? a : b));
     }
 
     /**
@@ -156,22 +153,22 @@ class TransactionAnalyzer {
      * @returns {string} The most common transaction type.
      */
     mostTransactionTypes() {
-        const typeCounts = {}; // объект-счетчик
+        const typeCounts = {};
         for (const t of this.transactions) {
-            if (typeCounts[t.transaction_type]) { // если счетчик для этого типа транзакции уже есть, то увеличивается на один
+            if (typeCounts[t.transaction_type]) {
                 typeCounts[t.transaction_type]++;
-            } else { // если счетчика нет, то устанавливается значение 1
+            } else { 
                 typeCounts[t.transaction_type] = 1;
             }
         }
-        const types = Object.keys(typeCounts); // ключи счетчика сохраняются в массив types 
+        const types = Object.keys(typeCounts);
         if (types.length === 1) { 
             return types[0];
         } else {
-            let mostCommonType = types[0]; // mostCommonType храненит тип транзакции с наибольшим количеством на данный момент и принимает значение первого элемента массива
+            let mostCommonType = types[0]; 
             for (const type of types) { 
                 if (typeCounts[type] > typeCounts[mostCommonType]) {
-                    mostCommonType = type; // если количество транзакций для текущего типа (type) больше, чем для текущего типа с наибольшим количеством (mostCommonType), то переменная mostCommonType обновляется на этот тип
+                    mostCommonType = type;
                 }
             }
             return mostCommonType;
@@ -184,8 +181,8 @@ class TransactionAnalyzer {
      * @returns {Array<Object>} Array of transactions before the specified date.
      */
     getTransactionsBeforeDate(date) {
-        const data = new Date(date); // вводимые данные date преобразуются в data
-        return this.transactions.filter(t => new Date(t.transaction_date) < data); // filter сохраняет только те элементы, которые равны true, тут те, которые меньше data
+        const data = new Date(date);
+        return this.transactions.filter(t => new Date(t.transaction_date) < data);
     }
 
     /**
@@ -194,7 +191,7 @@ class TransactionAnalyzer {
      * @returns {Object|null} Transaction object if found, otherwise null.
      */
     findTransactionById(id) {
-        return this.transactions.find((t) => t.transaction_id == id); // find ищет и возвращает первый элемент в массиве, где t_id равен true
+        return this.transactions.find((t) => t.transaction_id == id);
     }
 
     /**
@@ -202,13 +199,13 @@ class TransactionAnalyzer {
      * @returns {Array<string>} Array of transaction descriptions.
      */
     mapTransactionDescriptions() {
-        return this.transactions.map(t => t.transaction_description); // map позволяет применить эту функцию для каждого элемента массива и возвращает измененный массив
+        return this.transactions.map(t => t.transaction_description);
     }
 }
 
 
-const transactions = require('./transaction.json'); // импорт
-const analyzer = new TransactionAnalyzer(transactions); // создание экземпляра класса с транзакциями
+const transactions = require('./transaction.json'); 
+const analyzer = new TransactionAnalyzer(transactions);
 
 // первый метод - тип транзакции
 // console.log(analyzer.getUniqueTransactionType());
