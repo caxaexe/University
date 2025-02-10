b2348de13134ee9817b5b5edbd86e2d6cad9925d
caxaexe <fasvenomo@gmail.com>
2025-02-10 19:14:59 +0200

    Update index.php

--- PHP/Laborator1/index.php
+++ PHP/Laborator1/index.php
@@ -1,18 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1</title>
+</head>
+<body>
+    <div>
+        <h2>Первое задание</h2>
+        <?php echo "Привет, мир!"; ?>
+    </div>
+    <div>
+        <h2>Второе задание</h2>
+        <?php 
+        echo "Hello, World with echo! "; 
+        print "Hello, World with print!"; 
+        ?>
+    </div>
+    <div>
+        <h2>Третье задание</h2>
+        <?php
+            $days = 288;
+            $message = "Все возвращаются на работу!";
 
-echo "Привет, мир!"; 
-
-echo "<br>--------------------------------------------------------------<br>";
-
-echo "Hello, World with echo! ";
-echo "Hello, World with print!";
-
-echo "<br>--------------------------------------------------------------<br>";
-
-$days = 288;
-$message = "Все возвращаются на работу!";
-
-echo $days . $message;
-echo "<br> $days $message";
-
-echo "<br>--------------------------------------------------------------<br>";
+            echo "Прошло " . $days . " дней. ". $message;
+            echo "<br>Прошло {$days} дней. {$message}";
+        ?>
+    </div>
+</body>
+</html>
