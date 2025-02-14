75243bed5be7d9c67c98cd0734518179333f3d47
caxaexe <fasvenomo@gmail.com>
2025-02-14 16:07:55 +0200

    upd

---
+++ PHP/Project/index.php
@@ -0,0 +1,14 @@
+<?php
+define("PROJECT", "blog");
+
+require_once './components/header.php'; 
+
+?>
+
+    <main class="container mx-auto">
+        Hello, World! 
+    </main>
+
+<?php 
+require_once './components/footer.php'; ?>
+
