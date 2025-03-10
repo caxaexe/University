9d41f5f7e35577a1b6a3182d5e24d803edb797ea
caxaexe <fasvenomo@gmail.com>
2025-03-10 13:52:28 +0200

    upd

--- PHP/Laborator3/index2.php
+++ PHP/Laborator3/index2.php
@@ -17,17 +17,16 @@
             margin: 0;
             padding: 0;
             background-color: #f4f4f4;
+            text-align: center;
         }
         header, footer {
             background-color: rgb(200, 161, 53);
             color: white;
-            text-align: center;
             padding: 10px 0;
         }
         nav {
             background-color: rgb(171, 131, 52);
             overflow: hidden;
-            text-align: center;
         }
         nav a {
             color: white;
@@ -42,12 +41,13 @@
             display: grid;
             grid-template-columns: repeat(5, 1fr);
             gap: 15px;
-            padding: 20px;
+            padding: 10px;
             justify-content: center;
         }
         .nachos img {
-            width: 100%;
-            height: auto;
+            width: 270px;
+            height: 270px;
+            object-fit: cover;
             border-radius: 8px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
             border: 3px solid #c8a135;
@@ -57,6 +57,8 @@
             transform: scale(1.05);
         }
     </style>
+</head>
+<body>
 </head>
 <body>
 
