76ccb7541f05b36562e5560964b7a3079c2bea63
caxaexe <fasvenomo@gmail.com>
2025-04-10 18:48:25 +0300

    Удалена папка имя_папки

--- Java/Spring/Laborator1/src/main/java/com/example/demo/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.entities.Author;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> { }
