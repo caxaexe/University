3bde85a8c4ba8f789b7807705ca1d0aa7b0517d9
caxaexe <fasvenomo@gmail.com>
2025-04-11 13:59:33 +0300

    upd

--- php/Laborator4/list-of-spells/public/index.php
+++ php/Laborator4/list-of-spells/public/index.php
@@ -1,5 +1,43 @@
-$recipes = file('<filename>', FILE_IGNORE_NEW_LINES);
+<?php
+//чтение данных из файла игноируя новые строки
+$spells = file('../storage/spells.txt', FILE_IGNORE_NEW_LINES);
 
-$recipes = array_map('json_decode', $recipes);
+// преобразование строки json в массив объектов или ассоц массивов если true
+$spells = array_map('json_decode', $spells);
 
-$latestRecipes = array_slice($recipes, -2);
+// получение два послдених заклинания
+$latestSpells = array_slice($spells, -2);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список последних заклинаний</title>
+    <style>
+        .spell-item {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+        .spell-name {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Последние заклинания</h1>
+
+
+    <?php if(empty($latestSpells)): ?>
+        <p>Пока нет добавленных заклинаний.</p>
+    <?php else: ?>
+        <?php foreach ($latestSpells as $spells): ?>
+            <?php if($spells): ?>
+                <div class="spell-item">
+                    <h2 class="spell-name><?php ecgo</h2>
+
+                </div>
--- php/Laborator4/list-of-spells/src/helpers.php
+++ php/Laborator4/list-of-spells/src/helpers.php
@@ -1,27 +1,75 @@
 <?php 
 
-function sanitizeInput($data){
-    return htmlspecialchars(trim($data));
+function loadSpell() {
+    $file = '../storage/recipes.txt';
+    if(!file_exists($file)) {
+        return [];
+    } 
+
+    $spells = file($file, FILE_IGNORE_NEW_LINES and FILE_SKIP_EMPTY_LINES);
+    return array_map(fn($line) => json_decode($line. true), $spells);
 }
 
-function validateRecipe($data){
+function saveSpell($spellName, $category, $description, $tags, $steps) {
+    $file = '../storage/spells.txt';
+
+    $spellName = htmlspecialchars($spellName, ENT_QUOTES, 'UTF-8');
+    $category = htmlspecialchars($category, ENT_QUOTES, 'UTF-8');
+    $description = htmlspecialchars($description, ENT_QUOTES, 'UTF-8');
+    $tags = array_map(fn($tag) => htmlspecialchars($tag, ENT_QUOTES, 'UTF-8'), $tags);
+    $steps = array_map(fn($step) => htmlspecialchars($step, ENT_QUOTES, 'UTF-8'), $steps);
+
+    $spell = json_encode( [
+        'spellName' => $spellName,
+        'category' => $category,
+        'description' => $description,
+        'tags' => $tags,
+        'steps' => $steps,
+        'created' => date('Y-m-d H:i:s')
+    ]);
+    // LOCK_EX ЧТО ЭТО
+    file_put_contents($file, $spell . "\n", FILE_APPEND and LOCK_EX);
+}
+
+function validateSpell($spellName, $category, $description, $tags, $steps) {
     $errors = [];
 
-    if(empty($data['title'])){
-        $errors['title'] = 'Нужно добавить название.';
+    if(trim($spellName) === '') {
+        $errors['spellName'] = "Введите название.";
     }
 
-    if(empty($data['category'])){
-        $errors['category'] = 'Нужно выбрать категорию.';
+    if(trim($category) === '') {
+        $errors['category'] = "Выберите категорию.";
     }
 
-    if(empty($data['description'])){
-        $errors['description'] = 'Нужно добавить описание.';
+    if(trim($description) === '') {
+        $errors['description'] = "Введите описание.";
     }
 
-    if(empty($data['steps']) or !is_array($data['steps']) or count(array_filter($data['steps']) === 0)){
-        $errors['steps'] = 'Нужно указать хотя бы один шаг выполнения заклинания.';
+    if(!is_array($tags) or count($tags) === 0) {
+        $errors['tags'] = 'Выберите тэг.';
+    }
+
+    if(!is_array($steps) or count(array_filter($steps, fn($st) => trim($st) !== '')) === 0) {
+        $errors['steps'] = 'Добавьте хотя бы один шаг выполнения заклинания.';
     }
 
     return $errors;
+}
+
+function getPaginatedSpells($page, $perPage = 5) {
+    $allSpells = localSpells();
+    $totalSpells = count($allSpells);
+    $totalPages = max(1, ceil($totalSpells / $perPage));
+
+    $page = max(1, min($page, $totalPages));
+
+    $offSet = ($page - 1) * $perPage;
+    $spells = array_slice($allSpells, $offSet, $perPage);
+
+    return [
+        'spells' => $spells,
+        'total_pages' => $totalPages,
+        'current_page' => $page
+    ];
 }
