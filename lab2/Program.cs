795fde4367b1b380c7463d3941d2c836fbc2b7e2
caxaexe <fasvenomo@gmail.com>
2025-09-21 13:59:22 +0300

    add labs c#

---
+++ lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, Jerry");
+Console.WriteLine("--------------------------");
+ABC();
+Thread.Sleep(3000);
+ABC();
+Thread.Sleep(3000);
+ABC();
+Console.WriteLine("--------------------------");
+A();
+A();
+A();
+
+
+void ABC()
+{
+    Console.WriteLine("A");
+    Console.WriteLine("B");
+    Console.WriteLine("C");
+}
+
+void A()
+{
+    Console.WriteLine("A");
+    B();
+    C();
+}
+
+void B()
+{
+    Console.WriteLine("B");
+}
+
+void C()
+{
+    Console.WriteLine("C");
+}
+
+
