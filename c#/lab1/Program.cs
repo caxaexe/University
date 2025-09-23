ab0490f4ded06925cb567b1ccd486d0e0caf1de7
caxaexe <fasvenomo@gmail.com>
2025-09-23 13:41:24 +0300

    add labs

--- c#/lab1/Program.cs
+++ c#/lab1/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.WriteLine("2 + 3");
 Console.WriteLine("=");
 Console.WriteLine(2 + 3);
-Console.WriteLine(".");
 Console.WriteLine("-5");
 Console.WriteLine("=");
 Console.WriteLine(2 + 3 - 5);
